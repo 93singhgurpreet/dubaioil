cc21e9885bccc20d03341b1472f853525e80b8d8
gurpreet <you@example.com>
2021-03-19 15:36:28 +0530

    update settings4

--- oil_fact/settings.py
+++ oil_fact/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 
