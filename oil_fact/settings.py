0ed3da60f059264a37eb230e7241abdc61b6df66
gurpreet <you@example.com>
2021-03-19 15:42:30 +0530

    update settings5

--- oil_fact/settings.py
+++ oil_fact/settings.py
@@ -124,9 +124,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 
